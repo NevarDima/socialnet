12c387353cd48e03dcdab29c7d6709a83dfe93eb
Dima Nevar <nevar.dima@gmail.com>
2021-08-16 16:34:14 +0300

    deploy to heroku

---
+++ src/main/java/com/nevar/socialnet/domain/Role.java
@@ -0,0 +1,12 @@
+package com.nevar.socialnet.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
